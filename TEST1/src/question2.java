2fe2024ce168af4936e4bcd3ee285a946549c960
ayury79 <32505539+ayury79@users.noreply.github.com>
2017-10-15 07:26:57 +0800

    Update question2.java

--- TEST1/src/question2.java
+++ TEST1/src/question2.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class question2 {
     public static void main(String [] args) {
 
-        // The name of the file to open.
+        // The name of the file to open or read.
         String fileName = "A.txt";
         
         // The name of the file to create.
@@ -79,44 +79,3 @@ public class question2 {
         }
     }
 }
-
-/*public class Test {
-    public static void main(String [] args) {
-
-        // The name of the file to create.
-        String fileName = "B.BIN";
-
-        try {
-            // Put some bytes in a buffer so we can
-            // write them. Usually this would be
-            // image data or something. Or it might
-            // be unicode text.
-            String bytes = "Hello theren";
-            byte[] buffer = bytes.getBytes();
-
-            FileOutputStream outputStream =
-                new FileOutputStream(fileName);
-
-            // write() writes as many bytes from the buffer
-            // as the length of the buffer. You can also
-            // use
-            // write(buffer, offset, length)
-            // if you want to write a specific number of
-            // bytes, or only part of the buffer.
-            outputStream.write(buffer);
-
-            // Always close files.
-            outputStream.close();       
-
-            System.out.println("Wrote " + buffer.length + 
-                " bytes");
-        }
-        catch(IOException ex) {
-            System.out.println(
-                "Error writing file '"
-                + fileName + "'");
-            // Or we could just do this:
-            // ex.printStackTrace();
-        }
-    }
-}*/
